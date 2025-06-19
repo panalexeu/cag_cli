67cfe048d6e8178b2049afa7dbbf17d3030e8d3e
Oleksii Horhcynskyi <pan.alexeu@gmail.com>
2025-06-19 15:29:55 +0300

    defined deinit command

--- src/cli/app.py
+++ src/cli/app.py
@@ -1,17 +1,40 @@
 import os
-import typer
+import shutil
+from typing import Annotated
 
+import typer
+from typer import Exit, Option
 from rich import print
 
 app = typer.Typer()
 
+INIT_DIR = './.cag'
+
 
 @app.command()
 def init():
-    """Initializes .cag directory to store cache."""
-    if not os.path.exists('./.cag'):
-        os.mkdir('./.cag')
-        print('.cag directory was initialized')
+    """Creates a `.cag` directory to store cached files content."""
+    if not os.path.exists(INIT_DIR):
+        os.mkdir(INIT_DIR)
+        print('[bold green]`.cag`[/bold green] directory was initialized.')
+    else:
+        print('[bold green]`.cag`[/bold green] directory is already initialized.')
+        Exit(code=-1)
+
+
+@app.command(
+    name='deinit'
+)
+def de_init(
+        force: Annotated[bool, Option(prompt=True, )]
+):
+    """Removes a `.cag` directory, with all cached content inside."""
+    if not os.path.exists(INIT_DIR):
+        print('[bold green]`.cag`[/bold green] directory is not initialized.')
+        Exit(code=-1)
+    elif force:
+        shutil.rmtree(INIT_DIR)
+        print('[bold green]`.cag`[/bold green] directory was removed.')
 
 
 if __name__ == '__main__':
